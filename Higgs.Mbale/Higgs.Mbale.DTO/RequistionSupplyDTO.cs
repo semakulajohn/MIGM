c198c7077ed0a4acd049d353c43ea152776691a2
semakulajohn <jpolosemakula@gmail.com>
2021-09-08 15:44:28 -0700

    requistion suppy table
    Added requistion suppy table

---
+++ Higgs.Mbale/Higgs.Mbale.DTO/RequistionSupplyDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Higgs.Mbale.DTO
+{
+   public  class RequistionSupplyDTO
+    {
+        public long RequistionId { get; set; }
+        public long SupplyId { get; set; }
+        public System.DateTime TimeStamp { get; set; }
+
+    }
+}
---
+++ Higgs.Mbale/Higgs.Mbale.Database/Schema Objects/Schemas/dbo/Tables/RequistionSupply.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE [dbo].[RequistionSupply]
+(
+	[RequistionId] BIGINT NOT NULL,
+	[SupplyId] BIGINT NOT NULL,
+	
+	[TimeStamp] DATETIME NOT NULL,
+ CONSTRAINT [PK_dbo.RequistionSupply] PRIMARY KEY CLUSTERED 
+(
+	[RequistionId] ASC,
+	[SupplyId] ASC
+	
+) 
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[RequistionSupply]  ADD  CONSTRAINT [FK_dbo.RequistionSupply_dbo.Requistion_RequistionId] FOREIGN KEY([RequistionId])
+REFERENCES [dbo].[Requistion] ([RequistionId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[RequistionSupply] CHECK CONSTRAINT [FK_dbo.RequistionSupply_dbo.Requistion_RequistionId]
+GO
+
+ALTER TABLE [dbo].[RequistionSupply]  ADD  CONSTRAINT [FK_dbo.RequistionSupply_dbo.Supply_SupplyId] FOREIGN KEY([SupplyId])
+REFERENCES  [dbo].[Supply] ([SupplyId])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [dbo].[RequistionSupply] CHECK CONSTRAINT [FK_dbo.RequistionSupply_dbo.Supply_SupplyId]
+GO
+
